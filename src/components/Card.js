dc1acf2e3c353f4128b66dac1b2d99feeea3f628
bakemonostan <ehizomokhomion@gmail.com>
2022-04-04 04:30:16 -0700

    ui updated

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,16 +1,20 @@
 import { ReactComponent as PatternDivider } from '../images/pattern-divider-desktop.svg';
+import { ReactComponent as Dice } from '../images/icon-dice.svg';
 
 // styles
 import './Card.css';
 function Card() {
   return (
     <div className='cardContainer'>
-      <h3 className='advice'>Advice #</h3>
+      <h4 className='advice'>Advice #</h4>
       <p>
         Lorem ipsum, dolor sit amet consectetur adipisicing elit. Delectus,
         quia.
       </p>
       <PatternDivider />
+      <div className='diceContainer'>
+        <Dice className='dice' />
+      </div>
     </div>
   );
 }
---
+++ src/hooks/useFetch.js
@@ -0,0 +1,7 @@
+import { useState, useEffect } from 'react';
+
+export const useFetch = (url) => {
+  const [data, setData] = useState(null);
+  const [isPending, setIsPending] = useState(false);
+  const [error, setError] = useState(null);
+};
