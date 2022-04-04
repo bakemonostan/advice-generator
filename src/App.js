dd14351b595132881093e265669879ca4bd5dba3
bakemonostan <ehizomokhomion@gmail.com>
2022-04-04 03:26:12 -0700

    updated

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
 import './App.css';
+import Card from './components/Card';
 
 function App() {
   return (
     <div className='App'>
-      <header className='App-header'>Saaaaaaaap</header>
+      <Card />
     </div>
   );
 }
--- src/components/Card.js
+++ src/components/Card.js
@@ -1,7 +1,16 @@
+import { ReactComponent as PatternDivider } from '../images/pattern-divider-desktop.svg';
+
+// styles
+import './Card.css';
 function Card() {
   return (
-    <div>
-      <p>Card</p>
+    <div className='cardContainer'>
+      <h3 className='advice'>Advice #</h3>
+      <p>
+        Lorem ipsum, dolor sit amet consectetur adipisicing elit. Delectus,
+        quia.
+      </p>
+      <PatternDivider />
     </div>
   );
 }
