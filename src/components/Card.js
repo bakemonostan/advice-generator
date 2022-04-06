0d7e089cc641af746b508ac14cdd995e3918668b
bakemonostan <ehizomokhomion@gmail.com>
2022-04-06 08:58:15 -0700

    updated ui and removed useFetch hook

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,29 +1,48 @@
 import { ReactComponent as PatternDivider } from '../images/pattern-divider-desktop.svg';
+import { useEffect, useState } from 'react';
 import { ReactComponent as Dice } from '../images/icon-dice.svg';
-import { useFetch } from '../hooks/useFetch';
 
 // styles
 import './Card.css';
+
 function Card() {
-  const { data, isPending, error } = useFetch(
-    'https://jsonplaceholder.typicode.com/todos/1'
-  );
-
-  data.map((todo) => {
-    return todo.tilte;
+  const [randomAdvice, setRandomAdvice] = useState({
+    id: '',
+    advice: '',
   });
+  const [isLoading, setIsLoadng] = useState(false);
 
+  const fetchData = async () => {
+    setIsLoadng(true);
+    const response = await fetch('https://api.adviceslip.com/advice');
+    const { slip } = await response.json();
+
+    setRandomAdvice(slip);
+    setIsLoadng(false);
+  };
+
+  useEffect(() => {
+    fetchData();
+  }, []);
+
+  const getAdvice = () => {
+    fetchData();
+    console.log(randomAdvice.advice);
+  };
   return (
     <div className='cardContainer'>
-      <h4 className='advice'>Advice #</h4>
-      <p>
-        Lorem ipsum, dolor sit amet consectetur adipisicing elit. Delectus,
-        quia.
-      </p>
-      <PatternDivider />
-      <div className='diceContainer'>
-        <Dice className='dice' />
-      </div>
+      {isLoading ? (
+        <p>Loading...</p>
+      ) : (
+        <>
+          <h4 className='advice'>Advice #{randomAdvice.id}</h4>
+          <p>{randomAdvice.advice}</p>
+          <PatternDivider />
+          <div className='diceContainer'>
+            <Dice className='dice' onClick={getAdvice} />
+          </div>
+        </>
+      )}
     </div>
   );
 }
